77537a4c787f157b5031a555160b6cffbf424f52
Mamoru13 <HAOS6664@yandex.ru>
2020-12-31 16:48:31 +0300

    Зделал первое задание, второе не успел

---
+++ DZ_3/Complex.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DemoProject
+{
+    struct Complex
+    {
+        public double a, b;
+        public string c;
+
+
+        public Complex(double v1, double v2, string znack)
+        {
+            a = v1;
+            b = v2;
+            c = znack;
+        }
+
+        public void Print()
+        {
+            string temp = String.Empty;
+
+            if (a == 0) temp = $"";
+            else temp = $"{a}";
+
+            if (b == 0) temp += $"";
+            else if (b > 0) temp +=  $" {c} {b}i";
+            else temp += $" {c} {-b}i";
+            if (a == 0 && b == 0) temp = "0";
+
+
+            Console.WriteLine(temp);
+        }
+
+
+        //сложение
+        public static Complex Sum(Complex z1, Complex z2)
+        {
+            Complex res = new Complex(z1.a + z2.a, z1.b +z2.b, "+");
+
+            return res;
+        }
+        //Вычитание
+        public static Complex Vich(Complex z1, Complex z2)
+        {
+            Complex res = new Complex(z1.a - z2.a, z1.b - z2.b, "-");
+
+            return res;
+        }
+        //Умножение
+        public static Complex Prois(Complex z1, Complex z2)
+        {
+            Complex res = new Complex(z1.a * z2.a - z1.b * z2.b, z1.a * z2.b + z1.b * z2.a, "*");
+
+            return res;
+        }
+
+    }
+}
---
+++ DZ_3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+
+    enum MathVich : byte
+    {
+        Sum = 1,
+        Vichitanie = 2,
+        Proisvedenie = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
+            //б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+            //в) Добавить диалог с использованием switch демонстрирующий работу класса.
+            //Шурухин Александр Сергеевич
+
+            Complex z1 = new Complex(5, -6, "");
+
+            z1.Print();
+
+            Complex z2 = new Complex(-3, 2, "");
+
+            z2.Print();
+
+            MathVich level;
+            int user;
+            do
+            {
+                Console.Write("Произвести Сумму(Введите '1'), Вычитание(Введите '2') или произведение(Введите '3') комплексных чисел? ");
+                int.TryParse(Console.ReadLine(), out user); //проверка на строковый ввод      
+            } while (user < 1 || user > 3);
+
+            if (user == 1) level = MathVich.Sum;
+            else if (user == 2) level = MathVich.Vichitanie;
+            else level = MathVich.Proisvedenie;
+
+            switch (level)
+            {
+                case MathVich.Sum:
+                    //сумма комплексных чисел
+                    Console.WriteLine("Сумма комплексных чисел ");
+                    Complex z = Complex.Sum(z1, z2);
+                    z.Print();
+                    break;
+
+                case MathVich.Vichitanie:
+                    //вычитание комплексных чисел
+                    Console.WriteLine("Вычитание комплексных чисел ");
+                    Complex x = Complex.Vich(z1, z2);
+                    x.Print();
+                    break;
+
+                case MathVich.Proisvedenie:
+                    //произведение комплексных чисел
+                    Console.WriteLine("Произведение комплексных чисел ");
+                    Complex y = Complex.Prois(z1, z2);
+                    y.Print();
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.ReadKey(); //Задержка чёрного экрана
+        }
+    }
+}
